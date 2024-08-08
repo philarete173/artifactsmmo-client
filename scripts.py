3092580a5f4d028a4b4e7a92eba1d49c45da3aa4
Philipp Ulanovskiy <f.ulanovskii@bars.group>
2024-08-08 23:56:17 +0300

    Add scenarios for some level 10 crafts Refactoring of scenarios storage

--- scripts.py
+++ scripts.py
@@ -39,6 +39,30 @@ class ScenariosStorage:
             ],
         }
 
+    def _sell_item(self, item_name='', quantity=1):
+        self.character.move(5, 1)
+        self.character.sell_item(item_name, quantity)
+
+    def _craft_weapon(self, item_code='', quantity=1):
+        self.character.move(2, 1)
+        self.character.crafting(item_code, quantity)
+
+    def _craft_gear(self, item_code='', quantity=1):
+        self.character.move(3, 1)
+        self.character.crafting(item_code, quantity)
+
+    def _craft_jewelry(self, item_code='', quantity=1):
+        self.character.move(1, 3)
+        self.character.crafting(item_code, quantity)
+
+    def _craft_metal(self, item_code='', quantity=1):
+        self.character.move(1, 5)
+        self.character.crafting(item_code, quantity)
+
+    def _craft_planks(self, item_code='', quantity=1):
+        self.character.move(-2, -3)
+        self.character.crafting(item_code, quantity)
+
     def gather_copper_ore(self, quantity=1):
         self.character.move(2, 0)
         for _ in range(quantity):
@@ -77,122 +101,137 @@ class ScenariosStorage:
     def craft_copper(self, quantity=1):
         self.gather_copper_ore(6 * quantity)
 
-        self.character.move(1, 5)
-        self.character.crafting('copper', quantity)
+        self._craft_metal('copper', quantity)
 
     def craft_iron(self, quantity=1):
         self.gather_iron_ore(6 * quantity)
 
-        self.character.move(1, 5)
-        self.character.crafting('iron', quantity)
+        self._craft_metal('iron', quantity)
 
     def craft_steel(self, quantity=1):
         self.gather_iron_ore(2 * quantity)
         self.gather_coal(4 * quantity)
 
-        self.character.move(1, 5)
-        self.character.crafting('steel', quantity)
+        self._craft_metal('steel', quantity)
 
     def craft_gold(self, quantity=1):
         self.gather_gold_ore(6 * quantity)
 
-        self.character.move(1, 5)
-        self.character.crafting('gold', quantity)
+        self._craft_metal('gold', quantity)
 
     def craft_ash_planks(self, quantity=1):
         self.gather_ash_wood(6 * quantity)
 
-        self.character.move(-2, -3)
-        self.character.crafting('ash_plank', quantity)
+        self._craft_planks('ash_plank', quantity)
 
     def craft_spruce_planks(self, quantity=1):
         self.gather_spruce_wood(6 * quantity)
 
-        self.character.move(-2, -3)
-        self.character.crafting('spruce_plank', quantity)
+        self._craft_planks('spruce_plank', quantity)
 
     def craft_hardwood_planks(self, quantity=1):
         self.gather_ash_wood(2 * quantity)
         self.gather_birch_wood(4 * quantity)
 
-        self.character.move(-2, -3)
-        self.character.crafting('hardwood_plank', quantity)
-
-    def sell_item(self, item_name='', quantity=1):
-        self.character.move(5, 1)
-        self.character.sell_item(item_name, quantity)
+        self._craft_planks('hardwood_plank', quantity)
 
     def craft_wooden_staff(self, quantity=1, sell=False):
         self.craft_ash_planks(3 * quantity)
 
         self.gather_ash_wood(4 * quantity)
 
-        self.character.move(2, 1)
-        self.character.crafting('wooden_stick', quantity)
-        self.character.crafting('wooden_staff', quantity)
+        self._craft_weapon('wooden_stick', quantity)
+        self._craft_weapon('wooden_staff', quantity)
 
         if sell:
-            self.sell_item('wooden_staff', quantity)
+            self._sell_item('wooden_staff', quantity)
 
     def craft_wooden_shield(self, quantity=1, sell=False):
         self.craft_ash_planks(3 * quantity)
 
-        self.character.move(3, 1)
-        self.character.crafting('wooden_shield', quantity)
+        self._craft_gear('wooden_shield', quantity)
 
         if sell:
-            self.sell_item('wooden_shield', quantity)
+            self._sell_item('wooden_shield', quantity)
 
     def craft_copper_dagger(self, quantity=1, sell=False):
         self.craft_copper(3 * quantity)
 
-        self.character.move(2, 1)
-        self.character.crafting('copper_dagger', quantity)
+        self._craft_weapon('copper_dagger', quantity)
 
         if sell:
-            self.sell_item('copper_dagger', quantity)
+            self._sell_item('copper_dagger', quantity)
 
     def craft_copper_helmet(self, quantity=1, sell=False):
         self.craft_copper(3 * quantity)
 
-        self.character.move(3, 1)
-        self.character.crafting('copper_helmet', quantity)
+        self._craft_gear('copper_helmet', quantity)
 
         if sell:
-            self.sell_item('copper_helmet', quantity)
+            self._sell_item('copper_helmet', quantity)
 
     def craft_copper_boots(self, quantity=1, sell=False):
         self.craft_copper(3 * quantity)
 
-        self.character.move(3, 1)
-        self.character.crafting('copper_boots', quantity)
+        self._craft_gear('copper_boots', quantity)
 
         if sell:
-            self.sell_item('copper_boots', quantity)
+            self._sell_item('copper_boots', quantity)
 
     def craft_copper_ring(self, quantity=1, sell=False):
         self.craft_copper(4 * quantity)
 
-        self.character.move(1, 3)
-        self.character.crafting('copper_ring', quantity)
+        self._craft_jewelry('copper_ring', quantity)
 
         if sell:
-            self.sell_item('copper_ring', quantity)
+            self._sell_item('copper_ring', quantity)
 
     def craft_copper_armor(self, quantity=1, sell=False):
         self.craft_copper(5 * quantity)
 
-        self.character.move(3, 1)
-        self.character.crafting('copper_armor', quantity)
+        self._craft_gear('copper_armor', quantity)
 
         if sell:
-            self.sell_item('copper_armor', quantity)
+            self._sell_item('copper_armor', quantity)
 
     def craft_copper_legs_armor(self, quantity=1, sell=False):
         self.craft_copper(4 * quantity)
 
-        self.character.move(3, 1)
-        self.character.crafting('copper_legs_armor', quantity)
+        self._craft_gear('copper_legs_armor', quantity)
 
         if sell:
-            self.sell_item('copper_legs_armor', quantity)
+            self._sell_item('copper_legs_armor', quantity)
+
+    def craft_iron_dagger(self, quantity=1, sell=False):
+        self.craft_copper(2 * quantity)
+        self.craft_iron(6 * quantity)
+
+        self._craft_weapon('iron_dagger', quantity)
+
+        if sell:
+            self._sell_item('iron_dagger', quantity)
+
+    def craft_iron_sword(self, quantity=1, sell=False):
+        self.craft_iron(8 * quantity)
+
+        self._craft_weapon('iron_sword', quantity)
+
+        if sell:
+            self._sell_item('iron_sword', quantity)
+
+    def craft_greater_wooden_staff(self, quantity=1, sell=False):
+        self.craft_ash_planks(3 * quantity)
+        self.craft_spruce_planks(4 * quantity)
+
+        self._craft_weapon('greater_wooden_staff', quantity)
+
+        if sell:
+            self._sell_item('greater_wooden_staff', quantity)
+
+    def craft_iron_boots(self, quantity=1, sell=False):
+        self.craft_iron(6 * quantity)
+
+        self._craft_gear('iron_boots', quantity)
+
+        if sell:
+            self._sell_item('iron_boots', quantity)
